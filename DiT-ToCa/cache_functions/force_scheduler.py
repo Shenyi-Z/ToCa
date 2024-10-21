da6ae51723be5ffa5be30ada293387843ab0b9bf
Chang Zou <115690021+Shenyi-Z@users.noreply.github.com>
2024-10-22 01:54:33 +0800

    Update force_scheduler.py

--- DiT-ToCa/cache_functions/force_scheduler.py
+++ DiT-ToCa/cache_functions/force_scheduler.py
@@ -12,7 +12,7 @@ def force_scheduler(cache_dic, current):
     step_factor = torch.tensor(1 + linear_step_weight - 2 * linear_step_weight * current['step'] / current['num_steps'])
     threshold = torch.round(cache_dic['fresh_threshold'] / step_factor)
 
-    if (current['step'] in range(50,100)) and (cache_dic['fresh_ratio'] ！= 0):
+    if (current['step'] in range(50,100)) and (cache_dic['fresh_ratio'] != 0):
         # We find that in 50~100 steps, the model is extremely sensitive for cache, i.e. worse temporal redundancy.
         threshold = 2
 
